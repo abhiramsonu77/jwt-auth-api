0caf312e92d3a2c83246bf19454d904cd704484d
Abhiram <sonunair99@gmail.com>
2021-09-08 16:12:58 +0530

    DB updated

--- models/mongoConnect.js
+++ models/mongoConnect.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const initMongo = ()=>{
-    mongoose.connect('mongodb://localhost:27017/itemList')
+    mongoose.connect('mongodb+srv://zhastra:284472dem@cluster0.v5fvn.mongodb.net/APIREST?retryWrites=true&w=majority')
     .then(()=>{
     console.log('connected');
     }).catch(err=>{
